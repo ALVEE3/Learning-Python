dc8abc2ba8867928f26ae31da81fd43dae6eb77b
Alvee chowdhury <115806328+ALVEE3@users.noreply.github.com>
2025-09-03 18:43:15 +0600

    Add files via upload

---
+++ Hello, World!/Cal.py
@@ -0,0 +1,4 @@
+price = 100
+qty = 10
+total = price * qty
+print ('Total = '+ str(total))
---
+++ Hello, World!/Script.py
@@ -0,0 +1,6 @@
+print("Hello, World!.")
+myString = 'GoodBye'
+print(myString + "World!")
+myString2 = "Hi"
+print (myString2+ " " + myString  + ' World!')
+print (myString2 + ' ' + myString  + ' World!')
