32d28f6d73abe5cbf170be34c9517106ba0b822d
Alvee chowdhury <115806328+ALVEE3@users.noreply.github.com>
2025-09-04 16:26:58 +0600

    Add files via upload

---
+++ Hello, World!/Syntax.py
@@ -0,0 +1,16 @@
+# 1st part
+one = 'Alvee'
+two = 'Chowdhury'
+fullName = one +' '+two
+print(fullName)
+
+# 2nd part
+first = "Ahadul Islam" 
+state = ''' I am currenty studying Bsc in CSE at American International University-Bangladesh.
+I am in fanal semester of my graguation. And i just started learnging python by my self.'''
+print(first +' '+ two +' '+ state)
+
+# 3rd part
+Age = '; my age';age = 25
+print(first +' '+ two +' '+ Age +' '+ str(age))
+print(first+' '+ two +' '+ one) 
---
+++ Hello, World!/newLine.py
@@ -0,0 +1,11 @@
+one = "Hi, \nI am Alvee! \nWhat about you?"
+print(one)
+two = """Well hope you are doing great.
+I am doing good too.
+What about your family?"""
+print(two)
+three = '''I am fine too. I am learning python day by day and enjoying it.
+this is my day two of learning journey.'''
+print(three)
+#Lets print them all together
+print(one +" "+two+" " +three)
